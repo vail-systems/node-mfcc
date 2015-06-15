48bd7d44fe5a1725b08219bd6ddf75b62fb308f3
jjung <jjung@vailsys.com>
2015-06-15 13:35:24 -0500

    Updates to script to take in a FFT JSON file of FFT bins to process.

--- mfcc.js
+++ mfcc.js
@@ -9,6 +9,7 @@
 var program = require('commander'),
     Stats = require('fast-stats').Stats,
     fs = require('fs'),
+    path = require('path'),
     fjs = require('frequencyjs'),
     wav = require('wav'),
     Framer = require('./').Framer,
@@ -22,15 +23,23 @@ for (var k in u2pcm)
 }
 
 program.version('0.1')
-       .usage('[options] [wav] [out]')
-       .option('-v, --verbose', 'Supply to turn on verbose output. Default 0.', 0);
+       .usage('[options]')
+       .option('-v, --verbose', 'Supply to turn on verbose output. Default 0.', 0)
+       .option('-w, --wav [wav]', '[INPUT]: Supply a wave file to process.', undefined)
+       .option('-f, --fft [json]', '[INPUT]: Supply a JSON file of FFT bins to process (primarily for testing).', undefined);
 
 program.parse(process.argv);
 
-if (program.args.length != 1)
+if (program.wav === undefined && program.fft === undefined)
 {
-    console.log('Must provide a wave file.');
+    console.log('Must choose an input type from the program options.');
     program.outputHelp();
+    process.exit(1);
+}
+
+if (program.wav && program.fft)
+{
+    console.log('Please provide either a .wav file or a .json FFT file but not both!');
     process.exit(1);
 }
 
@@ -50,58 +59,90 @@ var freqAssigned = false;
 
 for (var i = 0; i < fftBins; i++) bins[i] = [];
 
-var wr = new wav.Reader(),
-    filterBank = mfcc.constructFilterBank(fftBins, nMelSpecBins, minFreq, maxFreq, sampleRate),
-    dct = new mfcc.DCT({
+/*-----------------------------------------------------------------------------------*\
+ * FFT JSON file
+\*-----------------------------------------------------------------------------------*/
+if (program.fft)
+{
+    var dct = new mfcc.DCT({
         lifter: undefined, 
         numCoefficients: 12
     });
 
-wr.on('data', function (buffer, offset, length) {
-    framer.frame(buffer, function (frame, fIx) {
-        var spectrum = fjs.toSpectrum(frame, {
-                sampling: sampleRate, 
-                method: 'fft'
-            }),
-            amplitudes = [];
+    // Assumes the file contains an array of FFT bins  
+    var amplitudes = JSON.parse(fs.readFileSync(path.resolve(program.fft)).toString());
+    amplitudes = amplitudes.map(parseFloat);
 
-        spectrum.forEach(function (spectra, ix) {
-            if (!freqAssigned) binsToFreq[ix] = spectra.frequency;
-            bins[ix].push(spectra.amplitude);
-            amplitudes.push(spectra.amplitude);
+    var filterBank = mfcc.constructFilterBank(amplitudes.length, nMelSpecBins, minFreq, maxFreq, sampleRate);
+
+    var freqPowers = mfcc.periodogram(amplitudes),
+        melSpec = filterBank(freqPowers);
+
+    var melCoefficients = dct.run(melSpec);
+    console.log(melCoefficients.join(','));
+    
+    //console.log(jsonFFTBins.length);
+    process.exit(1); 
+}
+
+/*-----------------------------------------------------------------------------------*\
+ * .wav file
+\*-----------------------------------------------------------------------------------*/
+if (program.wav)
+{
+    var wr = new wav.Reader(),
+        filterBank = mfcc.constructFilterBank(fftBins, nMelSpecBins, minFreq, maxFreq, sampleRate),
+        dct = new mfcc.DCT({
+            lifter: undefined, 
+            numCoefficients: 12
         });
 
-        console.log(spectrum);
-        var freqPowers = mfcc.periodogram(amplitudes),
-            melSpec = filterBank(freqPowers);
+    wr.on('data', function (buffer, offset, length) {
+        framer.frame(buffer, function (frame, fIx) {
+            var spectrum = fjs.toSpectrum(frame, {
+                    sampling: sampleRate, 
+                    method: 'fft'
+                }),
+                amplitudes = [];
 
-        var melCoefficients= dct.run(melSpec);
-        //console.log([fIx].concat(melCoefficients).join(','));
+            spectrum.forEach(function (spectra, ix) {
+                if (!freqAssigned) binsToFreq[ix] = spectra.frequency;
+                bins[ix].push(spectra.amplitude);
+                amplitudes.push(spectra.amplitude);
+            });
+
+            console.log(spectrum);
+            //var freqPowers = mfcc.periodogram(amplitudes),
+            //    melSpec = filterBank(freqPowers);
+
+            //var melCoefficients= dct.run(melSpec);
+            //console.log([fIx].concat(melCoefficients).join(','));
+        });
     });
-});
 
-wr.on('format', function (format) {
-    //TODO customize framer based on actual wave headers rather
-    // than assuming ulaw!
-    framer = new Framer({
-        map: u2pcm,
-        sizeS: framerSamples,
-        stepS: framerStep,
-        scale: win
+    wr.on('format', function (format) {
+        //TODO customize framer based on actual wave headers rather
+        // than assuming ulaw!
+        framer = new Framer({
+            map: u2pcm,
+            sizeS: framerSamples,
+            stepS: framerStep,
+            scale: win
+        });
     });
-});
 
-wr.on('end', function () {
-    statistics();
-    if (program.args.length == 2) writeToCSV();
-    var end = new Date().getTime();
-    console.log('Elapsed: ', end - start);
-    console.log(binsStats);
-    process.exit(1);
-});
+    wr.on('end', function () {
+        statistics();
+        if (program.args.length == 2) writeToCSV();
+        var end = new Date().getTime();
+        console.log('Elapsed: ', end - start);
+        console.log(binsStats);
+        process.exit(1);
+    });
 
-var start = new Date().getTime();
-fs.createReadStream(program.args[0]).pipe(wr);
+    var start = new Date().getTime();
+    fs.createReadStream(program.args[0]).pipe(wr);
+}
 
 function statistics() {
     bins.forEach(function (bin, ix) {
